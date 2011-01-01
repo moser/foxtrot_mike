c109f7f9d60f4fc5200763cd5db93de6a3152a91
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2011-01-01 18:41:18 +0100

    fixed tow flight spec

--- spec/models/tow_flight_spec.rb
+++ spec/models/tow_flight_spec.rb
@@ -3,18 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe TowFlight do
   it { should have_one :abstract_flight }
   
-  it "should not have a departure, from, cost_responsible of its own" do
-    TowFlight.generate! :abstract_flight => f = Flight.generate!
-    f.reload
-    f.launch.departure.should == f.departure
-    f.launch.departure = 1.week.ago
-    f.launch.departure.should == f.departure
-    f.launch.cost_responsible.should == f.cost_responsible
-    f.launch.from.should == f.from
-    f.launch.from = Airfield.generate!
-    f.launch.from_id = Airfield.generate!.id
-    f.launch.from.should == f.from
-    f.launch.from_id.should == f.from_id
+  it "should reset departure, from, cost_responsible when saved" do
+    t = TowFlight.spawn :abstract_flight => f = Flight.generate!
+    t.departure = 1.week.ago
+    t.from = Airfield.generate!
+    t.save
+    t.departure.should == f.departure
+    t.cost_responsible.should == f.cost_responsible
+    t.from.should == f.from
   end
   
 end
