a91547c26bb68477338f79e8f678cb9232e36633
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2011-12-16 17:33:11 +0100

    Whitespace fixes

--- app/models/tow_flight.rb
+++ app/models/tow_flight.rb
@@ -3,13 +3,13 @@ class TowFlight < AbstractFlight
   before_update :before_update_invalidate_accounting_entries
   after_update :after_update_invalidate_accounting_entries
   has_one :abstract_flight, :as => :launch, :readonly => false #the towed flight
-  
+
   include LaunchAccountingEntries
 
   def cost_hint
     abstract_flight.cost_hint
   end
-  
+
   def cost_responsible
     abstract_flight.cost_responsible #rescue nil
   end
@@ -17,19 +17,11 @@ class TowFlight < AbstractFlight
   def financial_account
     plane.financial_account
   end
-  
-#  def from
-#    abstract_flight.from rescue nil
-#  end
-#  
-#  def from_id
-#    abstract_flight.from_id rescue nil
-#  end
 
   def purpose
     @purpose ||= Purpose.get('tow')
   end
-  
+
   def initialize(*args)
     super(*args)
     if new_record?
@@ -46,7 +38,7 @@ private
       self.controller = abstract_flight.controller
     end
   end
-  
+
   def invalidation_necessary?
     !(changes.keys & ["plane_id", "departure_date", "departure_i", "arrival_i", "engine_duration", "cost_hint_id"]).empty?
   end
@@ -55,7 +47,7 @@ private
     self.accounting_entries_valid = false if invalidation_necessary? && abstract_flight.editable?
     true
   end
-  
+
   def after_update_invalidate_accounting_entries
     delay.create_accounting_entries if invalidation_necessary? && abstract_flight.editable? && !Rails.env.test? #HACK...
     # delay = false in test env sucks here, because when this is executed immediatly we get an infinite loop here.
--- spec/models/tow_flight_spec.rb
+++ spec/models/tow_flight_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + '/shared_examples_for_accounti
 describe TowFlight do
   it_behaves_like "an accounting entry factory"
   it { should have_one :abstract_flight }
-  
+
   it "should reset departure, from, cost_responsible when saved" do
     t = TowFlight.spawn :abstract_flight => f = Flight.generate!
     t.departure = 1.week.ago
@@ -14,7 +14,7 @@ describe TowFlight do
     t.cost_responsible.should == f.cost_responsible
     t.from.should == f.from
   end
-  
+
   it "should reset departure, from, cost_responsible when saved" do
     t = TowFlight.spawn :abstract_flight => f = Flight.generate!
     t.departure_date = 1.week.ago
@@ -27,7 +27,7 @@ describe TowFlight do
     t.cost_responsible.should == f.cost_responsible
     t.from.should == f.from
   end
-  
+
   describe "accounting_entries" do
     it "should invalidate accounting_entries and start a delayed job for their creation" do
       f = TowFlight.generate!
@@ -37,17 +37,17 @@ describe TowFlight do
       f.invalidate_accounting_entries
       f.accounting_entries_valid?.should be_false
     end
-    
+
     it "should create accounting entries without a delayed job, if called with delayed = false" do
       f = TowFlight.generate!
       f.accounting_entries
-      f.accounting_entries_valid?.should be_true 
+      f.accounting_entries_valid?.should be_true
       f.should_not_receive(:delay)
       f.should_receive(:create_accounting_entries)
       f.invalidate_accounting_entries(false)
       f.accounting_entries_valid?.should be_false #create_accounting_entries was mocked
     end
-    
+
     it "should not invalidate accounting_entries if not editable" do
       f = TowFlight.generate!
       f.accounting_entries
@@ -57,12 +57,12 @@ describe TowFlight do
       f.invalidate_accounting_entries(true)
       f.invalidate_accounting_entries(false)
     end
-    
+
     it "should create accounting entries if invalid" do
       f = TowFlight.generate!
       f.should_receive(:create_accounting_entries)
       f.accounting_entries
-    end  
+    end
   end
-  
+
 end
