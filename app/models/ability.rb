46bf53bc6412b48a2194e80a1803b97ad9f230c5
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2011-12-24 10:47:54 +0100

    Fixed rights for reader role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,25 +1,25 @@
-class Ability  
+class Ability
   include CanCan::Ability
 
   def self.roles
     [ :admin, :license_official, :treasurer, :controller, :reader ]
-  end  
-  
+  end
+
   def initialize(account)
     if account
       if account.role? :admin
         can :manage, :all
       else
-        if account.role?(:reader) || account.role?(:controller) || account.role?(:treasurer) || 
-           account.role?(:license_official) 
-          can :read, [Flight, :dashboards, :filtered_flights]
+        if account.role?(:reader) || account.role?(:controller) || account.role?(:treasurer) ||
+           account.role?(:license_official)
+          can :read, [Flight, TowFlight, :dashboards, :filtered_flights]
         end
         if account.role?(:controller)
           can [:read, :create], [Person, Plane, Airfield, WireLauncher, Flight, TowFlight]
           can :update, [Flight, TowFlight]
         end
         if account.role?(:treasurer)
-          can :manage, [  Person, Plane, Airfield, Flight, TowFlight, Group, 
+          can :manage, [  Person, Plane, Airfield, Flight, TowFlight, Group,
                           PersonCostCategory, PlaneCostCategory, WireLauncherCostCategory,
                           FinancialAccount, :cost_rules, CostHint,
                           FlightCostRule, WireLaunchCostRule, 
@@ -32,5 +32,5 @@ class Ability
         end
       end
     end
-  end  
-end 
+  end
+end
