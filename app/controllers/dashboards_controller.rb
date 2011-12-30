f9d40ce1c2ddfd0f8d3234cc0ae53d6722d9e2c4
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2011-12-30 12:57:16 +0100

    Show links to groups filtered flights

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,6 @@ class DashboardsController < ApplicationController
     @current_person = (current_account && current_account.person)
     @licenses = @current_person.licenses
     @planes = @current_person.group.planes
+    @groups = Group.all
   end
 end
