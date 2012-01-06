13ad7f965e3d3fd6cc7b63030533fe5cca366dae
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-01-06 12:17:33 +0100

    Seed and AppConfig

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 module AppConfig
-  HomeAirfield = Airfield.find_by_name('Cham')
+  HomeAirfield = Airfield.first
+#  HomeAirfield = Airfield.find_by_name("Name")
   Currency = '€'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ LegalPlaneClass.create! :name => "Kilo (TMG)"
 LegalPlaneClass.create! :name => "Kilo (SFL)"
 LegalPlaneClass.create! :name => "Echo"
 
-g = Group.create! :name => "Heimatgruppe (Umbennen?)"
+g = Group.create!(:name => "Heimatgruppe (Umbenennen?)")
 p = Person.create!(:firstname => "Admin", :lastname => "Admin", :group => g)
 Account.create!(:login => 'admin', :password => 'admin', :password_confirmation => 'admin', :person => p, :admin => true)
+Airfield.create!(:name => "Heimatplatz (Umbenennen?)")
