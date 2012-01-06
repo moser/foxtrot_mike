a1660547a1a6d30e9e61ea54bcab17487d42e0d9
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-01-06 14:18:45 +0100

    Fixed spec

--- spec/models/financial_account_spec.rb
+++ spec/models/financial_account_spec.rb
@@ -4,7 +4,7 @@ describe FinancialAccount do
   it { should have_many :financial_account_ownerships }
 
   it "should return name on to_s" do
-    f = FinancialAccount.generate!(:name => "lala")
-    f.to_s.should == "lala"
+    f = FinancialAccount.generate!(:name => "lala", :number => 123)
+    f.to_s.should == "lala (123)"
   end
 end
