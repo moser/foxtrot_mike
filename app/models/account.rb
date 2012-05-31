0c9e9418cafc02d9d734cec4da10311271339b59
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2012-05-31 12:02:50 +0200

    Moved test db to memory; HACK: load schema in account.rb, because schema needs to be loaded before call to acts_as_authentic. Specs now run in < 1m (~55m before)

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,14 @@
+if Rails.env == "test" &&
+   !defined?(SCHEMA_LOADED)
+   ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter &&
+   Rails.configuration.database_configuration['test']['database'] == ':memory:'
+  puts "creating sqlite in memory database"
+  silence_stream(STDOUT) do
+    load "#{Rails.root}/db/schema.rb"
+  end
+  SCHEMA_LOADED = true
+end
+
 class Account < ActiveRecord::Base
   acts_as_authentic
   belongs_to :person
