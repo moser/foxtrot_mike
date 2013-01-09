1e53e3697b999e8bb7b0ae0a6bd5eac839048237
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2013-01-09 22:51:23 +0100

    Cleaned PDFs

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -6,11 +6,12 @@ class PdfsController < ApplicationController
   end
 
   def render_pdf
-    render :pdf => "flights",
-           :template => "flights/_grouped.html.haml",
-           :disable_internal_links => true,
-           :disable_external_links => true,
-           :dpi => "90",
-           :orientation => "landscape"
+    render pdf: "flights",
+           template: "flights/_grouped.html.haml",
+           disable_internal_links: true,
+           disable_external_links: true,
+           dpi: "90",
+           orientation: "landscape",
+           margin: { top: 15, bottom: 15, left: 20, right: 20 }
   end
 end
