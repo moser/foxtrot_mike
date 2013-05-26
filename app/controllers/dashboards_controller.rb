87613b982e5a74ca615ca95982a19b6aef4baa4a
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2013-05-26 22:27:05 +0200

    Fixed dashboard for accounts without person

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,8 @@ class DashboardsController < ApplicationController
   def show
     authorize! :read, :dashboards
     @current_person = (current_account && current_account.person)
-    @licenses = @current_person.licenses
-    @planes = @current_person.group.planes
+    @licenses = @current_person.licenses if @current_person
+    @planes = @current_person.group.planes if @current_person
     @groups = Group.all
   end
 end
