8172be8f277fb6402ef7d111b398399f0b1d2b41
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2013-08-11 10:21:21 +0200

    Refactored string filter

--- app/models/string_flight_filter.rb
+++ app/models/string_flight_filter.rb
@@ -5,14 +5,23 @@ class StringFlightFilter < FlightFilter
 
   def flights
     if @filter_string && !@filter_string.blank?
-      @parent.flights.select do |flight|
-        @filter_string.split(',').map(&:strip).map do |filter|
-          method, value = filter.split(':').map(&:strip)
-          flight.send(method.to_sym).to_s =~ /#{value}/
-        end.reduce(&:"&&")
-      end
+      @parent.flights.select { |flight| matches?(flight) }
     else
       @parent.flights
+    end
+  end
+
+private
+  def matches?(flight)
+   filters.map do |filter|
+      flight.send(filter[:method]).to_s =~ /#{filter[:value]}/
+    end.reduce(&:"&&")
+  end
+
+  def filters
+    @filter_string.split(',').map(&:strip).map do |str|
+      method, value = str.split(':').map(&:strip)
+      { method: method.to_sym, value: value }
     end
   end
 end
