a2ccdeb6822ac55cca12b453d641b2439767a78a
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2014-01-11 16:47:52 +0100

    Added group to training stats

--- app/controllers/training_statistics_controller.rb
+++ app/controllers/training_statistics_controller.rb
@@ -2,14 +2,16 @@ class TrainingStatisticsController < ApplicationController
   def new
     authorize! :read, AbstractFlight
     @legal_plane_classes = LegalPlaneClass.all
+    @groups = Group.all
   end
 
   def create
     authorize! :read, AbstractFlight
     @year = params[:year]
     @legal_plane_class = LegalPlaneClass.find(params[:legal_plane_class_id])
+    @group = Group.find(params[:group_id])
     from = Date.parse("#{@year}-01-01")
     to = Date.parse("#{@year}-12-31")
-    @training_statistic = TrainingStatistics.new(Flight.where('departure_date >= ?', from).where('departure_date <= ?', to), @legal_plane_class)
+    @training_statistic = TrainingStatistics.new(@group.flights.where('departure_date >= ?', from).where('departure_date <= ?', to), @legal_plane_class)
   end
 end
