da2a56e17711af7e2189cd9fa44b83990cc95ff4
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2015-06-08 22:15:49 +0200

    Fix duplicate dashboard.

--- app/controllers/duplicate_dashboard_controller.rb
+++ app/controllers/duplicate_dashboard_controller.rb
@@ -3,8 +3,8 @@ class DuplicateDashboardController < ApplicationController
     authorize! :update, Person
     authorize! :update, Airfield
     authorize! :update, Plane
-    @from = params[:from_parsed] || 30.days.ago.to_date
-    @to = params[:to_parsed] || 0.days.ago.to_date
+    @from = params[:from] || 30.days.ago.to_date
+    @to = params[:to] || 0.days.ago.to_date
     @people = Person.where("created_at BETWEEN ? AND ?", @from, @to).where(duplicate_of_id: nil).all
     @airfields = Airfield.where("created_at BETWEEN ? AND ?", @from, @to).where(duplicate_of_id: nil).all
     @planes = Plane.where("created_at BETWEEN ? AND ?", @from, @to).where(duplicate_of_id: nil).all
