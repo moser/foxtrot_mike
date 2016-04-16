d8f3dd93a3714e593a357d0e642d54182f798667
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2016-04-16 18:04:59 +0200

    Extend balance sheet.

--- app/controllers/balance_sheets_controller.rb
+++ app/controllers/balance_sheets_controller.rb
@@ -2,7 +2,10 @@ class BalanceSheetsController < ApplicationController
   Tpl = Struct.new :account, :balance
   def show
     authorize! :read, FinancialAccount
+    @current_url = request.url
     @date = Date.today
+    sort = params[:sort] || "name"
+    raise "Unknown sort" unless %w{name balance}.include? sort
     @accounts = []
     if params[:report]
       @date = params[:report][:date]
@@ -11,7 +14,7 @@ class BalanceSheetsController < ApplicationController
       end.select do |obj|
         obj.balance != 0 and obj.account.number =~ /^[123]\d\d\d\d$/
       end.sort_by do |obj|
-        obj.account.name
+        obj.account.send(sort)
       end
     end
   end
