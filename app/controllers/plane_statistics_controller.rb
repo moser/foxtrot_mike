74b95a48689b62bf28b9e4582507cdf32a2a15a3
Martin Vielsmaier <martin.vielsmaier@gmail.com>
2017-03-21 21:05:29 +0100

    Extend plane stats.

--- app/controllers/plane_statistics_controller.rb
+++ app/controllers/plane_statistics_controller.rb
@@ -9,13 +9,19 @@ class PlaneStatisticsController < ApplicationController
       @group = Group.find(group_id)
       @planes = @group.planes
       res = ActiveRecord::Base.connection.execute(
-        "SELECT plane_id, cost_hint_id, count(1) as cnt, sum(arrival_i - departure_i) as duration
+        "SELECT plane_id, cost_hint_id, count(1) as cnt, sum(arrival_i - departure_i) as duration, count(distinct seat1_person_id) as pilot_count
          FROM abstract_flights
          WHERE departure_date BETWEEN '#{@year}-01-01' AND '#{@year}-12-31'
          GROUP BY plane_id, cost_hint_id"
       )
       @stats = res.group_by { |row| row['plane_id'] }
-      p @stats
+      res = ActiveRecord::Base.connection.execute(
+        "SELECT plane_id, count(distinct seat1_person_id) as pilot_count
+         FROM abstract_flights
+         WHERE departure_date BETWEEN '#{@year}-01-01' AND '#{@year}-12-31'
+         GROUP BY plane_id"
+      )
+      @pilot_stats = Hash[res.map { |row| [row['plane_id'], row['pilot_count']] }]
       @cost_hints = Hash[CostHint.all.map {|ch| [ch.id.to_s, ch]}]
     end
   end
